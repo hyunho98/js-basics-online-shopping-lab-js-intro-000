4e9f14191a88aa27083d0fa2385a14827ed76f1b
Alex Lee <hyunho98@gmail.com>
2019-05-03 23:48:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function addToCart(item) {
    itemName: [item],
    itemPrice: [Math.floor(Math.random() * 100) + 1];
  }
+ cart.push([item]);
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
