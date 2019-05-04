e81bde01a42d2a6273930ec7115e0f621657dfc9
Alex Lee <hyunho98@gmail.com>
2019-05-04 00:16:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,16 +12,23 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var num = Math.floor(Math.random() * 100) + 1;
+
  item = {
    itemName: item,
    itemPrice: num
  };
  cart.push(item);
+
  return `${item.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  var output = "In your cart, you have ";
+  for (var i = 0; i > cart.length - 1; i++){
+    output = output + `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+  }
+  output = output + `and ${cart[i+1].itemName} at $${cart[i+1].itemPrice}.`;
 }
 
 function total() {
