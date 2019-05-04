aee64e0080ddf788fa062915a53d67af9fd8e868
Alex Lee <hyunho98@gmail.com>
2019-05-04 00:56:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else {
-    var total = this.total();
+    var total = total();
     cart = [];
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
   }
